9fce3dbf12b63cbc5e88b70de7aa5191c0a53d10
Claude Ni <49127719+ClaudeNi@users.noreply.github.com>
2022-01-22 18:22:09 +0200

    Add files via upload

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import { useEffect, useRef, useState } from "react";
+import axios from "axios";
 import "./App.css";
 
 function App() {
@@ -6,20 +7,93 @@ function App() {
     const [input1, setInput1] = useState();
     const [input2, setInput2] = useState();
     const [input3, setInput3] = useState();
+    const [input4, setInput4] = useState();
+    const [select, setSelect] = useState("Select");
 
     const inputRef1 = useRef();
     const inputRef2 = useRef();
     const inputRef3 = useRef();
+    const inputRef4 = useRef();
     const selectRef = useRef();
 
     useEffect(() => {
         selectRef.current.addEventListener("change", () => {
-            console.log(selectRef.current.selectedOptions[0].text);
+            const tempSelect = selectRef.current.selectedOptions[0].text;
+            setSelect(tempSelect);
+            switch (tempSelect) {
+                case "Get all users":
+                    disableInputs([inputRef1, inputRef2, inputRef3, inputRef4]);
+                    break;
+                case "Get specific user":
+                    disableInputs([inputRef2, inputRef3, inputRef4]);
+                    break;
+                case "Add a user":
+                    disableInputs([inputRef2]);
+                    break;
+                case "Delete a user":
+                    disableInputs([inputRef2, inputRef3, inputRef4]);
+                    break;
+                case "Withdraw cash":
+                    disableInputs([inputRef2, inputRef4]);
+                    break;
+                case "Deposit cash":
+                    disableInputs([inputRef2, inputRef4]);
+                    break;
+                case "Transfer cash":
+                    disableInputs([inputRef4]);
+                    break;
+                default:
+                    break;
+            }
         });
     });
 
     const handleSend = (e) => {
         e.preventDefault();
+        switch (select) {
+            case "Get all users":
+                fetchData("");
+                break;
+            case "Get specific user":
+                if (inputRef1.current.value.length === 0) {
+                    setText("Please fill in the ID in input ID 1");
+                } else {
+                    fetchData(`/${inputRef1.current.value}`);
+                }
+                break;
+            case "Add a user":
+                if (inputRef1.current.value.length === 0) {
+                    setText("Please fill in the ID in input ID 1");
+                } else {
+                    updateData();
+                }
+                break;
+            case "Delete a user":
+                break;
+            case "Withdraw cash":
+                break;
+            case "Deposit cash":
+                break;
+            case "Transfer cash":
+                break;
+            default:
+                setText("Please select the action you want to make.");
+                break;
+        }
+    };
+
+    const fetchData = async (id) => {
+        const fetchedData = await axios.get(`/users${id}`);
+    };
+
+    const updateData = async () => {};
+
+    const deleteData = async () => {};
+
+    const disableInputs = (inputs) => {
+        inputs.forEach((input) => {
+            input.current.disabled = true;
+        });
     };
 
     return (
@@ -45,6 +119,7 @@ function App() {
                         ref={inputRef1}
                         onChange={(e) => setInput1(e.target.value)}
                         value={input1}
+                        disabled
                     ></input>
                     <label>ID 2: </label>
                     <input
@@ -52,13 +127,23 @@ function App() {
                         ref={inputRef2}
                         onChange={(e) => setInput2(e.target.value)}
                         value={input2}
+                        disabled
                     ></input>
-                    <label>Money: </label>
+                    <label>Cash: </label>
                     <input
                         type={"number"}
                         ref={inputRef3}
                         onChange={(e) => setInput3(e.target.value)}
                         value={input3}
+                        disabled
+                    ></input>
+                    <label>Credit: </label>
+                    <input
+                        type={"number"}
+                        ref={inputRef4}
+                        onChange={(e) => setInput4(e.target.value)}
+                        value={input4}
+                        disabled
                     ></input>
                     <input
                         type={"button"}
