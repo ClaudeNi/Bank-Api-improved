23fe7c7b5b2761c31877462e9831e82b9b82d061
Claude Ni <49127719+ClaudeNi@users.noreply.github.com>
2022-01-22 20:01:41 +0200

    Add files via upload

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -2,5 +2,5 @@ const mongoose = require("mongoose");
 const keys = require("../../config/keys");
 
 mongoose.connect(
-    `mongodb+srv://BankUsers:${keys.CONNECT_PASS}@freecluster.nk9f1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
+    `mongodb+srv://BankUsers:${keys.CONNECT_PASS}@freecluster.nk9f1.mongodb.net/BankAPI?retryWrites=true&w=majority`
 );
